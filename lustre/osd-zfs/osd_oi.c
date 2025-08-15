d5c18fb6babcc5601807c01494b9d36bf214ac61
alvaromartin990 <alvaromartingrande@gmail.com>
2025-08-15 09:58:30 +0200

    stage 4 fix

--- lustre/osd-zfs/osd_oi.c
+++ lustre/osd-zfs/osd_oi.c
@@ -1112,6 +1112,8 @@ struct osd_idmap_cache *osd_idc_find_or_init(const struct lu_env *env,
 	CDEBUG(D_INODE, "%s: FID "DFID" not in the id map cache\n",
 	       osd->od_svname, PFID(fid));
 
+	// stage 4
+	printk(KERN_ALERT "Stage 4: osd_idmap_cache OI Mapping\n");
 	/* new mapping is needed */
 	idc = osd_idc_add(env, osd, fid);
 	if (IS_ERR(idc)) {
